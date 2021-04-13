5b10d0c9eb3311c35f9ce343ffc64b6f271369d6
arcadia.youlten <arcadia.youlten@students.salle.url.edu>
2021-04-13 18:47:44 +0200

    created many files, tested everything in main, F16

--- main.cpp
+++ main.cpp
@@ -20,6 +20,56 @@ return flag;
 }
 */
 
+//ext2 --> page 11 smagic
+int command_line_error_check(int argc, char *argv[])
+{
+    int flag = 0;
+    std::string command_wanted = "/info";
+    //std::ifstream in(filename);
+
+    if (argc < 3 || argc > 3)
+    {
+        cout << "Error! Too few or too many arguments!" << endl;
+        return flag = 1;
+    }
+    //check if we're entering the correct command
+    if (command_wanted.compare(argv[1]) != 0)
+    {
+        cout << "Unknown Command." << endl;
+        cout << "Try entering /info" << endl;
+        return flag = 1;
+    }
+    //check the file type
+    else
+    {
+        FILE *file;
+        int fat16;
+        if (file = fopen(argv[2], "r"))
+        {
+            fclose(file);
+            printf("Opening %s...", argv[2]);
+
+            //check if FAT16
+            fseek(file, OFFSET_FAT16, SEEK_SET);
+            fread(&fat16, SIZE_FAT16, ELEMENTS_TO_READ_FAT16, file);
+            if (fat16 != 0)
+            {
+                cout << "Filetype is FAT16" << endl;
+                return flag = 0;
+            }
+            else
+            {
+                cout << "What the heck is this" << endl;
+                return flag = 1;
+            }
+        }
+        else
+        {
+            cout << "File Failed to Open" << endl;
+            return flag = 1;
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
