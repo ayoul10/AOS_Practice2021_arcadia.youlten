6ada395b116747a7564316c446f7b79d3f1a12ab
arcadia.youlten <arcadia.youlten@students.salle.url.edu>
2021-05-21 12:27:04 +0200

    Test for fat16 4th branch

--- FileFat16.cpp
+++ FileFat16.cpp
@@ -212,5 +212,5 @@ void FileFat16::findFat16File(char *filename, char *diskname)
 
 void FileFat16::deleteFat16FileFromDisk(char *filename, char *diskname)
 {
-    std::cout << "Not Implemented Yet" << endl;
+    std::cout << "In progress" << endl;
 }
